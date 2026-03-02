5eaca7dc8d35be616f5365dcea11976a094e2956
Your Name <you@example.com>
2026-03-02 22:03:06 +0000

    Fix Betty style for 3-islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  * _islower - checks for lowercase character
- * @c: The character to check
+ * @c: character to check
  *
  * Return: 1 if lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-    if (c >= 'a' && c <= 'z')
-        return (1);
-    return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
 }
