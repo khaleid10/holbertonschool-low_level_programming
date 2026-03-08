c3456573590a644a0f5da297ad73181a5b788fd6
khaleid10 <khaledaloraini@outlook.com>
2026-03-08 21:10:06 +0000

    Add function rev_string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+	char temp;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	j = i - 1;
+	i = 0;
+
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+
+		i++;
+		j--;
+	}
+}
