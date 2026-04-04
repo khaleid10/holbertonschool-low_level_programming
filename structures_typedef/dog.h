c060cc6a267244e4a99147c1536c8d768a0794e8
اØkhaleid10 <khaledaloraini@outlook.com>
2026-04-04 19:47:05 +0000

    fix dog.h and add new_dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,4 @@
-3#ifndef DOG_H
+#ifndef DOG_H
 #define DOG_H
 
 /**
@@ -15,11 +15,12 @@ struct dog
 };
 
 /**
- * typedef dog_t - new name for struct dog
+ * dog_t - new name for struct dog
  */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
