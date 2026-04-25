1e26f2d1ee3e759ab4e6e7bc29ca5606b2ee8a76
khaleid10 <khaledaloraini@outlook.com>
2026-04-25 20:43:01 +0000

    fix: add Betty documentation for print_list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list_t list
+ *
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
@@ -12,8 +18,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 
 	return (count);
