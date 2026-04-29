f985ce79bb8a8a4481ec26d6ae89d916104d7ec7
khaleid10 <khaledaloraini@outlook.com>
2026-04-29 14:47:27 +0000

    feat: create hash_tables.h header

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,33 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
+
+/**
+ * struct hash_node_s - hash node
+ * @key: key
+ * @value: value
+ * @next: pointer to next node
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - hash table
+ * @size: size of the array
+ * @array: array of linked lists
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+/* function prototypes */
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
