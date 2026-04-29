59da10faced186e6f7da455f867b4c95043ce71b
khaleid10 <khaledaloraini@outlook.com>
2026-04-29 15:00:43 +0000

    feat: implement key_index function

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key
+ * @key: the key
+ * @size: the size of the array
+ *
+ * Return: index where the key should be stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	hash = hash_djb2(key);
+	return (hash % size);
+}
